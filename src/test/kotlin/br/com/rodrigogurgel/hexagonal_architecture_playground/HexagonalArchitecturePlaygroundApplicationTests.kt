80c02b919a0142c549048d506b42502f23c26aa9
Rodrigo Gurgel <rodrigohenrique.gurgel@outlook.com>
2024-06-12 20:41:06 -0300

    spring initializr

---
+++ src/test/kotlin/br/com/rodrigogurgel/hexagonal_architecture_playground/HexagonalArchitecturePlaygroundApplicationTests.kt
@@ -0,0 +1,13 @@
+package br.com.rodrigogurgel.hexagonal_architecture_playground
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class HexagonalArchitecturePlaygroundApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
